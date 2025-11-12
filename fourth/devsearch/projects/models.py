e50ad6d4cf353c08ee7e681757e1aabaede16402
Sancho <sanjartilebaldiev00@gmail.com>
2025-11-12 21:43:18 +0300

    django7

--- fourth/devsearch/projects/models.py
+++ fourth/devsearch/projects/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from users.models import Profile
 
 # Create your models here.
 
 
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     featured_image = models.ImageField(upload_to="projects/%Y/%m/%d/", default="default.jpg")
